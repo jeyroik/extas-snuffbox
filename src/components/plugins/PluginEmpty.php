a7751abe99606586661280110483de12d2f72b18
jeyroik <jeyroik@gmail.com>
2020-05-27 14:07:52 +0300

    upg empty plugin

--- src/components/plugins/PluginEmpty.php
+++ src/components/plugins/PluginEmpty.php
@@ -9,10 +9,13 @@ namespace extas\components\plugins;
  */
 class PluginEmpty extends Plugin
 {
+    public static bool $worked = false;
+
     /**
      * @param mixed ...$args
      */
     public function __invoke(...$args)
     {
+        self::$worked = true;
     }
 }
